c3139ac9bacd59436f04d11b97d71d135d2c7c56
moviesindeep <145245026+moviesindeep@users.noreply.github.com>
2023-09-17 11:40:43 +0530

    Add files via upload

---
+++ movies/main.js
@@ -0,0 +1,6 @@
+// watch the evil dead rise ending scene
+document.getElementById("watch=the_evil_dead_rise_ending_scene").addEventListener("click", function(){
+  window.open("movies/the_evil_dead_rise_ending_scene.html");
+});
+
+
